f1f7beae13fa0e47da42aa003fee68f54e309e8e
Bobby Shannon <rshannon@buffalo.edu>
2016-02-28 12:54:29 -0500

    API token validation, api_token provider now valid along with e-mail
    Sync uid and api_token; skip uid presence validation when api_token used
    
    Validation tweak
    
    Validation tweak
    
    Validation tweak
    
    Validation tweak
    
    Validation tweak

--- app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
+++ app/models/devise_token_auth/concerns/user_omniauth_callbacks.rb
@@ -3,26 +3,33 @@ module DeviseTokenAuth::Concerns::UserOmniauthCallbacks
 
   included do
     validates :email, presence: true, email: true, if: Proc.new { |u| u.provider == 'email' }
-    validates_presence_of :uid, if: Proc.new { |u| u.provider != 'email' }
+    validates :api_token, presence: true, api_token: true, if: Proc.new { |u| u.provider == 'api_token' }
+    validates_presence_of :uid, if: :is_token_or_email?
 
-    # only validate unique emails among email registration users
-    validate :unique_email_user, on: :create
+    # only validate unique emails and api tokens
+    validate :unique_token_or_email, on: :create
 
-    # keep uid in sync with email
+    # keep uid in sync with email or api_token
     before_save :sync_uid
     before_create :sync_uid
   end
 
   protected
 
-  # only validate unique email among users that registered by email
-  def unique_email_user
+  def is_token_or_email?
+    provider != 'email' && provider != 'api_token'
+  end
+
+  def unique_token_or_email
     if provider == 'email' and self.class.where(provider: 'email', email: email).count > 0
       errors.add(:email, I18n.t("errors.messages.already_in_use"))
+    elsif provider == 'api_token' and self.class.where(provider: 'api_token', api_token: api_token).count > 0
+      errors.add(:api_token, I18n.t("errors.messages.already_in_use"))
     end
   end
 
   def sync_uid
     self.uid = email if provider == 'email'
+    self.uid = api_token if provider == 'api_token'
   end
 end
