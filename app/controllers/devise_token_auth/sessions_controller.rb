04eaab4fbd28c9ea5b6e48c978785bd7a53337b6
Bobby Shannon <rshannon@buffalo.edu>
2016-04-23 22:49:37 -0400

    Find by api token

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -12,6 +12,7 @@ module DeviseTokenAuth
       # Check
       field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
       api_token = request_authentication_header_value
+      api_token_valid = false
 
       @resource = nil
       if field
@@ -29,9 +30,15 @@ module DeviseTokenAuth
 
         @resource = resource_class.where(q, q_value).first
       elsif api_token
-        @resource = resource_class.where(api_token: api_token).first
+        resource_class.find_each do |resource|
+          if resource.valid_api_token(api_token)
+            @resource = resource
+            api_token_valid = true
+          end
+
+        end
       end
-      if api_token_or_username_password_valid?(api_token, field, q_value) and (!@resource.respond_to?(:active_for_authentication?) or @resource.active_for_authentication?)
+      if api_token_valid or username_password_valid?(field, q_value) and (!@resource.respond_to?(:active_for_authentication?) or @resource.active_for_authentication?)
         # create client id
         @client_id = SecureRandom.urlsafe_base64(nil, false)
         @token     = SecureRandom.urlsafe_base64(nil, false)
@@ -106,8 +113,8 @@ module DeviseTokenAuth
       request.headers[DeviseTokenAuth.default_authentication_header]
     end
 
-    def api_token_or_username_password_valid?(api_token, field, q_value)
-      ((@resource.valid_api_token?(api_token)) or (@resource and valid_params?(field, q_value) and @resource.valid_password?(resource_params[:password])))
+    def username_password_valid?(field, q_value)
+      (@resource and valid_params?(field, q_value) and @resource.valid_password?(resource_params[:password])))
     end
 
     def render_new_error
